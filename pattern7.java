ba3416f7d73ecfb3ec15013a292c81b11db2a2bf
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-13 13:35:01 +0530

    pattern7

--- pattern7.java
+++ pattern7.java
@@ -1,5 +1,15 @@
 public class pattern7 {
     public static void main(String[] args) {
-        
+        int n= 6;
+        for ( int i =1 ; i<=n ;i++){
+            for(int sp =1 ; sp<=i-1 ;sp++){
+                System.out.print("-");
+            }
+            for (int j = 1; j <= (2 * n - i-(i-1)); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
     }
 }
