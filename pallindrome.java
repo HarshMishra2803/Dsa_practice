3ae03648af45112b75e7834db9753016c4ff689a
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-13 21:14:20 +0530

    pallindrome

---
+++ pallindrome.java
@@ -0,0 +1,20 @@
+public class pallindrome{
+    public static boolean reverse (int n ){
+
+        int original = n ;
+
+        int ans = 0 ; 
+        while(n!=0){
+            int digit = n%10 ;
+            ans = ans *10 + digit ;
+            n = n/10;
+        }
+        return ans == original;
+        
+    }
+    public static void main(String[] args) {
+        int n = -121 ; 
+        System.out.println(reverse(n));
+    
+    }
+}
