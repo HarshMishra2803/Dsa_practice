805f78b4fd29adfa3d5ebede92b738ec48079d84
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-13 12:41:49 +0530

    pattern

--- pattern6.java
+++ pattern6.java
@@ -1,5 +1,15 @@
 public class pattern6 {
     public static void main(String[] args) {
+        int n = 6 ;
+        for ( int i = 0 ; i <n ; i++){
+            for ( int j = 0 ; j<n-i-1 ; j++){
+                System.out.print("_");
+            }
+            for ( int k=0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         
     }
 }
