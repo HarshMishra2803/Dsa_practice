a8713630a26ef91a19b3980d5a541c571967b96c
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-13 14:32:25 +0530

    pattern9

---
+++ pattern8.java
@@ -0,0 +1,29 @@
+public class pattern8 {
+    public static void main(String[] args) {
+        int n = 5 ;
+
+        // part1
+        for ( int i = 1 ; i<=5 ; i++){
+            for ( int j = 1 ; j<=n-i ; j++){
+                System.out.print("_");
+            }
+            for(int k = 1 ; k<= 2*i-1 ; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // part2
+
+        for( int i = 5 ; i>=1 ; i--){
+            for ( int j = n-i ; j>=1 ; j--){
+                System.out.print("_");
+            }
+            for(int k = 2*i-1 ; k>=1 ; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
