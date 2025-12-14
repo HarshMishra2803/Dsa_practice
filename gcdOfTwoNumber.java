0d5d3d52ac6b47199f18287ce8810d2eebfbee50
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-14 18:40:48 +0530

    done

---
+++ gcdOfTwoNumber.java
@@ -0,0 +1,23 @@
+public class gcdOfTwoNumber {
+
+    // bruteforce approach
+    public static int gcd(int N1, int N2) {
+        int gcd = 1;
+        int min = Math.min(N1, N2);
+
+        for (int i = 1; i <= min; i++) {
+            if (N1 % i == 0 && N2 % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        int N1 = 12;
+        int N2 = 24;
+
+        int result = gcd(N1, N2);   // function ka result store karo
+        System.out.println(result); // print karo
+    }
+}
---
+++ gcdOfTwoNumberappraoch2.java
@@ -0,0 +1,19 @@
+public class gcdOfTwoNumberappraoch2 {
+
+    public static int gcd(int N1, int N2) {
+        while (N2 != 0) {
+            int rem = N1 % N2;
+            N1 = N2;
+            N2 = rem;
+        }
+        return N1;
+    }
+
+    public static void main(String[] args) {
+        int N1 = 9;
+        int N2 = 12;
+
+        int result = gcd(N1, N2);
+        System.out.println(result);
+    }
+}
