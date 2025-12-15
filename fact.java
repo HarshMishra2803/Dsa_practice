f9b9ccc844e699caa22af25bb7fd7fe3031afba6
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-16 00:12:51 +0530

    rec

--- fact.java
+++ fact.java
@@ -1,21 +1,16 @@
-public class fact{
+public class fact {
     public static int factorial(int n) {
-        if (n == 0) {
-            return 0;
+        if (n == 0 || n==1) {
+            return 1;
         }
-    //     int mul=1; //iterative way
-    //     for(int i = 1 ; i<=n;i++){
-    //         mul = mul*i;
-    //     }
-    //    return mul;
+        // int mul=1; //iterative way
+        // for(int i = 1 ; i<=n;i++){
+        // mul = mul*i;
+        // }
+        // return mul;
 
-
-    // recursive way
-    int fact =1 ; 
-    fact = fact *(n-1);
-    
-
-
+        // recursive way
+        return  n* factorial(n - 1);
     }
 
     public static void main(String[] args) {
@@ -24,4 +19,3 @@ public class fact{
         System.out.println(result);
     }
 }
- 
