c00c10b8787c09e19e21138cee9f04cee61b47cf
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-20 00:32:46 +0530

    sort

---
+++ BubbleSortRecursive.java
@@ -0,0 +1,28 @@
+public class BubbleSortRecursive {
+
+    public static void bubbleSort(int[] arr, int n) {
+        // base case
+        if (n == 1) return;
+
+        // one pass of bubble sort
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+        // recursive call for remaining array
+        bubbleSort(arr, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {13, 46, 24, 52, 20, 9};
+        bubbleSort(arr, arr.length);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ SelectionSortt.java
@@ -0,0 +1,29 @@
+public class SelectionSortt {
+    public static void ss(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int curridx = i;
+
+            // minimum element dhundhna
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[curridx] > arr[j]) {
+                    curridx = j;
+                }
+            }
+
+            // ek hi baar swap
+            int temp = arr[curridx];
+            arr[curridx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        ss(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
