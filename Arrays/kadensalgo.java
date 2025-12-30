096700efb050bdb4ea9cf8a508aa7b61bc0375be
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-30 20:34:52 +0530

    done

--- Arrays/kadensalgo.java
+++ Arrays/kadensalgo.java
@@ -2,41 +2,36 @@ package Arrays;
 
 public class kadensalgo {
 
-    public static int  ka(int arr[]){
-        int max = Integer.MIN_VALUE;
-        int sum = 0 ;
-        int ansstart = -1;
-        int ansend = -1;
-        int start =0;
-        
-        
-        for ( int i = 0 ; i<arr.length;i++){
-            
+    public static int ka(int arr[]) {
+        int max = arr[0];
+        int sum = 0;
+        int ansStart = 0, ansEnd = 0, start = 0;
 
-            if(sum ==0 ) start = i ;
-            sum = sum + arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
 
-            if(sum>max){
-                max = sum ;
-                ansstart = start;
-                ansend = i ;
-            }
-            if(sum<0){
-                sum = 0 ;
-            }
-            if(max < 0){
-                max = 0 ;
+            if (sum > max) {
+                max = sum;
+                ansStart = start;
+                ansEnd = i;
             }
 
+            // Reset when sum goes negative
+            if (sum < 0) {
+                sum = 0;
+                start = i + 1;
+            }
         }
-        return max;
 
+        System.out.println("Subarray Start Index: " + ansStart);
+        System.out.println("Subarray End Index: " + ansEnd);
+        return max;
     }
+
     public static void main(String[] args) {
-        int arr [] = {-2,-3,4,-1,-2,1,5,-3} ;
+        int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
 
         int res = ka(arr);
-        System.out.println(res);
-        
+        System.out.println("Maximum Subarray Sum = " + res);
     }
 }
