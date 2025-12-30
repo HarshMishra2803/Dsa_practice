f9f082c5be3af368d19c9bc7ee48d9bb15bae7a7
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-30 20:13:25 +0530

    dome

--- Arrays/kadensalgo.java
+++ Arrays/kadensalgo.java
@@ -5,15 +5,22 @@ public class kadensalgo {
     public static int  ka(int arr[]){
         int max = Integer.MIN_VALUE;
         int sum = 0 ;
+        int ansstart = -1;
+        int ansend = -1;
+        int start =0;
+        
         
         for ( int i = 0 ; i<arr.length;i++){
+            
+
+            if(sum ==0 ) start = i ;
             sum = sum + arr[i];
 
             if(sum>max){
                 max = sum ;
+                ansstart = start;
+                ansend = i ;
             }
-
-
             if(sum<0){
                 sum = 0 ;
             }
@@ -30,5 +37,6 @@ public class kadensalgo {
 
         int res = ka(arr);
         System.out.println(res);
+        
     }
 }
