c2cff348ccb43c29811dc87921cdde8ea99703e5
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-01-18 20:14:45 +0530

    done

--- Arrays/pascaltriangleq1.java
+++ Arrays/pascaltriangleq1.java
@@ -2,22 +2,29 @@ package Arrays;
 
 public class pascaltriangleq1 {
 
+
+
+
     public static int nCr(int n, int r){
-        int res = 1;
-        int j = n-1;
-        int k = r-1;
-        
-        for(int i =0 ;i<k ;i++){
-            res = res*(j-i);
+
+        int res = 1 ;
+
+        for(int i = 0 ;i< r ;i++){
+            res = res * (n-i);
             res = res/(i+1);
         }
         return res;
+
     }
+
 
     public static void main(String[] args) {
         int n = 5 ; 
         int r = 3;
-        int ans = nCr(n,r);
+
+
+        
+        int ans = nCr(n-1,r-1);
         System.out.println(ans);
     }
     
