1d662017027ab083d6ceb35876a1f20abac4108c
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-02-04 19:36:42 +0530

    done

---
+++ pyhton/harsh.py
@@ -0,0 +1,85 @@
+# a = int(input("enter"))
+# b = int(input("enter"))
+# c = int(input("enter"))
+# d= int(input("enter"))
+# e = int(input("enter"))
+# sum = a+b+c+d+e
+# avg = sum /5
+# print(avg)
+
+
+# principle = int(input("enter the principle"))
+# rate = int(input("enter the rate"))
+# time= int(input("enter the time"))
+
+# simpleInterst = principle * rate * time/100
+# print(simpleInterst,type(simpleInterst))
+
+
+# car = int ( input( "enter the speed in km"))
+# time = int(input("enter the time of car"))
+
+# avergae_speed = car/time
+# print(avergae_speed)
+# s = input("Enter your Name")
+# print(len(s))
+# print(s.count("h"))
+# print(s[::-1])
+# print(s.endswith("a"))
+# print(s.startswith("m"))
+# print(s.capitalize())
+# print(s.upper())
+# print(s.title())
+# print(s.strip())
+# print(s.find("a"))
+# print(s.)
+
+# num  = int ( input ( "enter your number"))
+
+# if(num%2==0):
+#     print("number is even")
+# else:
+#     print("the number is odd")    
+
+# # Leap Year
+# num = int(input("enter the year"))
+# if((num%4==0 or num%400==0) and num%100!=0
+#    ):
+#     print("leap year")
+# else:
+#     print("not a leap year") 
+
+# num1 = int(input("enter"))       
+# num2 = int(input("enter"))   
+
+# if(num1 == num2):
+#     print("the numbers are equal")
+# else:
+#     print("the number are not safe")    
+
+
+# Greatest Among the three number
+# num1 = int(input("enter the number"))
+# num2= int(input("enter the number"))
+# num3 = int(input("enter the number"))
+
+# if(num1>num2):
+#     if(num1>num3):
+#         print("Num1 is the greatest")    
+# elif(num2>num1):
+#     if(num2>num3):
+#         print("Num2 is the Greatest")
+# else:
+#     print("Num3 is the Greatest")   
+
+
+# USING AND
+# if(num1>num2 and num1>num3):
+#     print("num1 is the largest")
+# elif(num2>num1 and num2>num3):
+#     print("Num2 is the greatest")    
+# else:
+#     print("Num3 is the greatest")  
+  
+
+num = int(input("Enter the Number"))
---
+++ pyhton/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+a = int(input("enter"))
+# b = int(input("enter"))
+# c = int(input("enter"))
+# d= int(input("enter"))
+# e = int(input("enter"))
+# sum = a+b+c+d+e
+# avg = sum /5
+# print(avg)
