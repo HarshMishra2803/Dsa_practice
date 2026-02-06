03f5b83d165f53f0181b6bfb4be14821daab1649
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-02-06 22:28:56 +0530

    done

--- pyhton/harsh.py
+++ pyhton/harsh.py
@@ -82,4 +82,107 @@
 #     print("Num3 is the greatest")  
   
 
-num = int(input("Enter the Number"))
+# num = int(input("Enter the Number"))
+
+# list1 = []
+# list1 = ["harsh","harsh1","harsh2","harsh3","harsh5"] # hetrogenous value store hoti h
+# print(list1[::])
+# print(list1[:-1])
+# print(list1.extend("harsh"))
+
+# name = ["aman","akash","arya","aditya","Aarav"]
+# print(max(name))
+# print(min(name))
+
+
+# list = [1,2,3,4,5]
+# print(list[:1])
+# print(list[-1::])
+# print(len(list))
+
+# length = len(list)
+# print(min(list))
+# print(max(list))
+# total = sum(list)
+
+# avg =total/length
+
+# print(avg) 
+
+
+
+# student = {
+#     "name":"akash",
+#     "age" : "18",
+#     "rollno":"20",
+#     "subject":["phyiscs","chemistrt","biology"],
+#     "marks":[20,30,40]
+# }
+# print(student.pop("name"))
+# print(student)
+
+# student["name"] = "harsh"
+# print(student)
+
+
+# country = {
+#     "India" : " Delhi",
+#     "Russia" : "mosqo",
+#     "USA" : "washingtonDc",
+#     "Pakistan": "islambad",
+#     "Bangladesh":"dhaka"
+# }
+
+# print(country.get("India"))
+# country["Japan"] = "Tokyo"
+# print(country)
+
+# country["USA"] = "NewYork"
+# print(country)
+
+# print(country.pop("Pakistan"))
+# print(country)
+
+# # list of dictionary
+
+# students = [
+#     {"name" : "Alice","age":20},
+#     {"name" : "bob","age":21},
+#     {"name" : "charlie","age":19},
+# ]
+# print(students[0]["age"])
+
+# for i in range(0,3):
+#     print(students)
+# # for i in range(100):
+# #     print("hello chauhan")    
+
+# str1 = "HarshMishra"
+# for char in str1:
+#     print(char, end =" ")
+
+# Pattern   
+# *
+# **
+# ***
+# ****
+
+row = 6 
+
+# for i in range(1, row + 1):
+#     print("*" * i)
+
+n = 4
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end = " ")
+
+    print()    
+
+ #    *
+ #   * *
+ #  * * *  
+ #  * * * *   
+
+ 
+
