7137c222946c0023022e18865e825dfbc39c690e
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-02-16 19:39:05 +0530

    done

--- Arrays/contest2ques.java
+++ Arrays/contest2ques.java
@@ -14,7 +14,7 @@ public class contest2ques {
             }
             
             if(sum ==k){
-                return count; //return
+                return count; //returnd
             }
         }
         return -1;    
