9c77d08c01849adc7c2dc812f19ca6b9c8f0383a
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-03-08 19:58:31 +0530

    done

--- BookAllocation.java
+++ BookAllocation.java
@@ -29,7 +29,7 @@ public class BookAllocation {
 
         for (int i = 0; i < n; i++) {
             sum += arr[i];
-            max = Math.max(max, arr[i]); // math function
+            max = Math.max(max, arr[i]); 
         }
 
         int start = max;
