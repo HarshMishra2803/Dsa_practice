77be4adc5578ad5eca568e8ec31f8a3a0aecb89e
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-03-16 22:54:08 +0530

    done

--- BookAllocation.java
+++ BookAllocation.java
@@ -39,7 +39,7 @@ public class BookAllocation {
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (isPossible(arr, n, m, mid)) { //poss
+            if (isPossible(arr, n, m, mid)) { 
                 ans = mid;
                 end = mid - 1; 
             } else {
